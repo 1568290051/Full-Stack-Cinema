3f5be3dd7c691bc179fcfcfa279db66b80053103
wenkai <1695050320@qq.com>
2019-10-16 08:46:36 +0800

    10-16 8:46修改

---
+++ priject/src/App.vue
@@ -0,0 +1,214 @@
+<template>
+  <div id="app">
+    <el-container>
+      <!-- 头部 -->
+      <!-- 导航 start -->
+      <el-header style="height: 60px;padding-left: 0px;padding-right: 0px;">
+        <el-row>
+          <el-col :span="24">
+            <div class="nav">
+              <!-- 中心 -->
+              <div class="type_area">
+                <el-menu
+                  class="el-menu-demo"
+                  mode="horizontal"
+                  unique-opened="true"
+                  background-color="black"
+                  text-color="#fff"
+                  active-text-color="#ffd04b"
+                >
+                  <el-menu-item index="1">首页</el-menu-item>
+                  <el-submenu :index="item.path" v-for="item in menuList" :key="item.id">
+                    <template slot="title">{{item.name}}</template>
+                    <el-menu-item
+                      :index="item2.path"
+                      v-for="item2 in item.subObject"
+                      :key="item2.subObjectId"
+                    >{{item2.subName}}</el-menu-item>
+                  </el-submenu>
+                  
+                </el-menu>
+                 <el-input placeholder="输入关键字搜索"  v-model="search" style="width:156px;  position: fixed;
+  top: 12px;right:60px;" >
+                <i slot="prefix" class="el-input__icon el-icon-search"></i>
+              </el-input>
+              </div>
+         
+             
+            </div>
+            
+          </el-col>
+        </el-row>
+
+        <!-- 导航 end-->
+      </el-header>
+      <!-- 中部 start -->  <!-- 去除两边空隙 -->
+      <el-main style="height: 60px;padding-left: 0px;padding-right: 0px;padding-bottom: 5px;border-top-width: 0px;padding: 0px">
+        <router-view />
+      </el-main>
+      <!-- 中部 end -->
+      <!-- 底部 start -->
+      <el-footer style="height: 60px;padding-left: 0px;padding-right: 0px;">
+        <!-- 去除两边空隙 -->
+        <el-row>
+          <el-col>
+            <!-- 底部 -->
+            <div class="Bottom">
+              <!-- 底部版心 -->
+              <div class="Bottom_type_area">
+                <div class="disclaimer">
+                  免责声明:本站所有视频均来自互联网收集而来，版权归原创者所有，如果侵犯了你的权益，请通知我们，我们会及时删除侵权内容，谢谢合作。流量统计站长统计
+                  <div>Copyright © 2019-2019 《免费电影_OK资源》</div>
+                </div>
+                <div class="copyright"></div>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </el-footer>
+      <!-- 底部 end -->
+    </el-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      //菜单数据 -假数据
+      menuList: [
+        {
+          id: 1,
+          name: "连续剧",
+          path: "e21",
+          subObject: [
+            {
+              subObjectId: 5431,
+              subName: "战争片",
+              path: "13"
+            },
+            {
+              subObjectId: 43,
+              subName: "战争片",
+              path: "pathsd"
+            },
+            {
+              subObjectId: 54,
+              subName: "战争片",
+              path: "e1"
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "电影片",
+          path: "e22",
+          subObject: [
+            {
+              subObjectId: 41,
+              subName: "战争片",
+              path: "QDQW"
+            },
+            {
+              subObjectId: 312,
+              subName: "战争片",
+              path: "WQf"
+            },
+            {
+              subObjectId: 321,
+              subName: "战争片",
+              path: "qwdw"
+            }
+          ]
+        },
+        {
+          id: 3,
+          name: "连续剧",
+          path: "e23",
+          subObject: [
+            {
+              subObjectId: 113,
+              subName: "战争片",
+              path: "dsad"
+            },
+            {
+              subObjectId: 3321,
+              subName: "战争片",
+              path: "pathdqw"
+            },
+            {
+              subObjectId: 3213,
+              subName: "战争片",
+              path: "dwqd"
+            }
+          ]
+        }
+      ],
+      // 搜索内容
+      search: ""
+    };
+  }
+};
+</script>
+
+<style>
+/* 初始化 */
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+.el-container {
+  width: 100%;
+  height: 100%;
+}
+
+/* 导航样式 */
+.nav {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 60px;
+  background-color: black;
+  z-index: 100;
+}
+.type_area {
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: #f0f8ff;
+}
+/* 中间区域样式 */
+.el-main {
+  background-color: #f0f8ff;
+}
+/* 底部 */
+.Bottom {
+  background-color: #f0f8ff;
+  width: 100%;
+  height: 100px;
+}
+/* 底部版心 */
+.Bottom_type_area {
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: #e9e8ef;
+}
+/* 免责声明 */
+.disclaimer {
+  padding: 20px 0;
+  color: #777777;
+  font-size: 12px;
+  border-top: 1px solid #555;
+  background: #e9e8ef;
+  text-align: center;
+  line-height: 30px;
+}
+
+</style>
---
+++ priject/src/views/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- 首页 -->
+    <div class="box_viewpage">
+      <!-- 版心 -->
+      <div class="viewpage_type_area )">
+        <ul class="viewpage_type_area_left">
+          <li>132</li>
+          <li>312</li>
+          <li>321</li>
+          <li>312</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style >
+/* 轮播图 */
+.box_viewpage {
+  height: 350px;
+  background-color: black;
+  opacity: 0.9;
+}
+/* 版心 */
+.viewpage_type_area {
+  overflow: hidden;
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: antiquewhite;
+}
+ul {
+  list-style: none;
+}
+/* 左边轮播图 */
+.viewpage_type_area_left {
+  position: relative;
+  width: 960px;
+  height: 310px;
+}
+viewpage_type_area_left li {
+  position: absolute;
+  top: 0;
+  left: 0;
+  float: left;
+  /* display: inline-block; */
+  width: 100%;
+  height: 310px;
+  background-color: aqua;
+}
+</style>
